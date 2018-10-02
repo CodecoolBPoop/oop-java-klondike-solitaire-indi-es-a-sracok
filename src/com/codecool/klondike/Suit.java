6d48d59c9c38aa0d4b35618635395b9b42f32088
Szajler Indira <szajler.indira@gmail.com>
2018-10-02 16:06:23 +0200

    color added to Suit enum

--- src/com/codecool/klondike/Suit.java
+++ src/com/codecool/klondike/Suit.java
@@ -1,15 +1,22 @@
 package com.codecool.klondike;
 
 public enum  Suit {
-    HEARTS("hearts"), DIAMONDS("diamonds"), SPADES("spades"), CLUBS("clubs");
+    HEARTS("hearts", "red"), DIAMONDS("diamonds", "red"), SPADES("spades", "black"), CLUBS("clubs", "black");
 
     private final String cardSuit;
 
-    private Suit(String cardSuit){
+    private  final String color;
+
+    private Suit(String cardSuit, String color){
         this.cardSuit=cardSuit;
+        this.color=color;
     }
 
     public String getCardSuit() {
         return cardSuit;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
