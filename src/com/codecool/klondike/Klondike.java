563a5a52e0046152a820004c86694727ae9bb0d8
David Szentesi <szntsd@gmail.com>
2018-10-04 16:38:07 +0200

    theming button

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -3,7 +3,6 @@ package com.codecool.klondike;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
@@ -18,9 +17,8 @@ public class Klondike extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Card.loadCardImages();
+        Card.loadCardImages("card_images/card_back.png");
         Game game = new Game();
-        Card card = new Card();
         game.setTableBackground(new Image("/table/green.png"));
 
         primaryStage.setTitle("Klondike Solitaire");
@@ -32,6 +30,8 @@ public class Klondike extends Application {
 
         themeButton.setOnAction((event) -> {
             game.setTableBackground(new Image("/table/teddy_bg.png"));
+            // Card.cardBackImage = new Image("card_images/card_back_rainbow.png");
+            Card.loadCardImages("card_images/card_back_rainbow.png");
         });
 
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
